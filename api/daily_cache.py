339f2aa96f917f6ac153dd5ab80014d15d8b712d
aymather <aymather@gmail.com>
2022-03-13 16:56:21 -0700

    test daily cache script

--- api/daily_cache.py
+++ api/daily_cache.py
@@ -4,3 +4,16 @@ import pandas as pd
 import numpy as np
 import time
 
+db_string = 'postgresql+psycopg2://postgres:Wheninrome1;@database-1.couue7ynblpf.us-west-1.rds.amazonaws.com:5432/postgres'
+
+client = ClientBase(db_string)
+
+string = """
+    select *
+    from shazamablam.artists
+    where name ~* :artist
+"""
+params = { 'artist': 'doja cat' }
+df = client.execute(string, params)
+
+print(df)
